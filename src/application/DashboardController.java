8ede2532c581937a7b98ff3c6c963d116a73b806
BatuhanU <batuhan.usluel@gmail.com>
2018-04-05 20:01:15 -0300

    Added all the columns in table

--- src/application/DashboardController.java
+++ src/application/DashboardController.java
@@ -46,14 +46,6 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 public class DashboardController {
-    private final ObservableList<Person> data
-    = FXCollections.observableArrayList(
-            new Person("Jacob", "Smith"),
-            new Person("Isabella", "Johnson"),
-            new Person("Ethan", "Williams"),
-            new Person("Emma", "Jones"),
-            new Person("Michael", "Brown")
-    );
 	 @FXML private TableView<Person> tableView = new TableView<Person>();
 	
 	@FXML
@@ -66,14 +58,40 @@ public class DashboardController {
     @SuppressWarnings("unchecked")
 	@FXML
 	 private void tableEnable() {
+    	final ObservableList<Person> data
+        = FXCollections.observableArrayList(
+        		new Person("AverageTrading","BTC","OMG", "Bittrex", "28.2(5:30)", "NA", "Running","000000"),
+        		new Person("Something","BTC","OMG", "EXCHNAGE", "28.2(5:30)", "NA", "Running","1111111"),
+        		new Person("AverageTrading","BTC","OMG", "Bittrex", "28.2(5:30)", "NA", "Running","222222"),
+        		new Person("Yes","BTC","OMG", "Bittrex", "28.2(5:30)", "NA", "Running","333333"),
+        		new Person("SuchProfit","MuchWow","Doge", "Bittrex", "28.2(5:30)", "NA", "Running","44444")
+        );
 		System.out.println("TABLE2!");
-		
-		TableColumn<Person, String> firstNameCol = new TableColumn<Person, String>("First Name");
-        firstNameCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));
-        
-        TableColumn<Person, String> lastNameCol = new TableColumn<Person, String>("Last Name");
-        lastNameCol.setCellValueFactory(new PropertyValueFactory<>("lastName"));
 
+		TableColumn<Person, String> OrderTypeCol = new TableColumn<Person, String>("Order Type");
+		OrderTypeCol.setCellValueFactory(new PropertyValueFactory<>("OrderType"));
+        
+        TableColumn<Person, String> BasePairCol = new TableColumn<Person, String>("Base Pair");
+        BasePairCol.setCellValueFactory(new PropertyValueFactory<>("BasePair"));
+
+        TableColumn<Person, String> AltPairCol = new TableColumn<Person, String>("Alt Pair");
+        AltPairCol.setCellValueFactory(new PropertyValueFactory<>("AltPair"));
+        
+        TableColumn<Person, String> ExchangesCol = new TableColumn<Person, String>("Exchanges");
+        ExchangesCol.setCellValueFactory(new PropertyValueFactory<>("Exchanges"));
+        
+        TableColumn<Person, String> StartTimeCol = new TableColumn<Person, String>("Start Time");
+        StartTimeCol.setCellValueFactory(new PropertyValueFactory<>("StartTime"));
+        
+        TableColumn<Person, String> EndTimeCol = new TableColumn<Person, String>("End Time");
+        EndTimeCol.setCellValueFactory(new PropertyValueFactory<>("EndTime"));
+        
+        TableColumn<Person, String> RunningCol = new TableColumn<Person, String>("Running");
+        RunningCol.setCellValueFactory(new PropertyValueFactory<>("Running"));
+        
+        TableColumn<Person, String> OrderIDCol = new TableColumn<Person, String>("OrderID");
+        OrderIDCol.setCellValueFactory(new PropertyValueFactory<>("OrderID"));
+        
         TableColumn<Person, String> actionCol = new TableColumn<Person, String>("Action");
         Callback<TableColumn<Person, String>, TableCell<Person, String>> cellFactory
                 = //
@@ -93,8 +111,7 @@ public class DashboardController {
                             btn.setOnAction(event -> {
                                 Person person = getTableView().getItems().get(getIndex());
                                 
-                                System.out.println(person.getFirstName()
-                                        + "   " + person.getLastName());
+                                System.out.println(person.getOrderID());
                             });
                             setGraphic(btn);
                             setText(null);
@@ -109,45 +126,72 @@ public class DashboardController {
         String css = this.getClass().getResource("/assets/tableview.css").toExternalForm();
         tableView.getStylesheets().setAll(css);
         tableView.setItems(data);
-        tableView.getColumns().addAll(firstNameCol, lastNameCol, actionCol);
+        tableView.getColumns().addAll(OrderTypeCol, BasePairCol, AltPairCol,ExchangesCol,StartTimeCol,EndTimeCol,RunningCol,OrderIDCol,actionCol);
 	}
     
 	@FXML
 	private void tableUpdate() {
-		final ObservableList<Person> data2
-        = FXCollections.observableArrayList(
-                new Person("Jacob", "Smith"),
-                new Person("Isabella", "Smith"),
-                new Person("Ethan", "Smith"),
-                new Person("Michael", "BroSmithSmithwn")
-        );
+		ObservableList<Person> data2 = FXCollections.observableArrayList();
+		data2.addAll(new Person("Market Making","BTC","ETH", "Bittrex", "28.2(5:30)", "NA", "Running","111111"));
+		data2.addAll(new Person("Arbitrage","BTC","DCR", "Poloniex & Bittrex", "28.2(5:30)", "NA", "Running","22222"));
+		data2.addAll(new Person("QuickBuy","BTC","XRP", "Binance", "28.2(5:30)", "28.2(5:30)", "Stopped" , "33333"));
+		data2.addAll(new Person("AverageTrading","BTC","OMG", "Bittrex", "28.2(5:30)", "NA", "Running", "44444"));
 		tableView.setItems(data2);
 		tableView.refresh();
 	}
     public static class Person {
-
-        private final SimpleStringProperty firstName;
-        private final SimpleStringProperty lastName;
-        private Person(String fName, String lName) {
-            this.firstName = new SimpleStringProperty(fName);
-            this.lastName = new SimpleStringProperty(lName);
+    	private final SimpleStringProperty OrderType;
+    	private final SimpleStringProperty BasePair;
+    	private final SimpleStringProperty AltPair;
+    	private final SimpleStringProperty Exchanges;
+    	private final SimpleStringProperty StartTime;
+    	private final SimpleStringProperty EndTime;
+    	private final SimpleStringProperty Running;
+    	private final SimpleStringProperty OrderID;
+        private Person(String OrderType,String BasePair,String AltPair, String Exchanges, String StartTime, String EndTime, String Running, String OrderID) {
+			this.OrderType = new SimpleStringProperty(OrderType);
+			this.BasePair  = new SimpleStringProperty(BasePair);
+			this.AltPair = new SimpleStringProperty(AltPair);
+			this.Exchanges = new SimpleStringProperty(Exchanges);
+			this.StartTime = new SimpleStringProperty(StartTime);
+			this.EndTime = new SimpleStringProperty(EndTime);
+			this.Running = new SimpleStringProperty(Running);
+			this.OrderID = new SimpleStringProperty(OrderID);
+        }
+        
+        public String getOrderType() {
+            return OrderType.get();
         }
 
-        public String getFirstName() {
-            return firstName.get();
+        public void setOrderType(String fName) {
+        	OrderType.set(fName);
         }
-
-        public void setFirstName(String fName) {
-            firstName.set(fName);
+        
+        public String getOrderID() {
+            return OrderID.get();
         }
-
-        public String getLastName() {
-            return lastName.get();
+        
+        public String getBasePair() {
+            return BasePair.get();
         }
-
-        public void setLastName(String fName) {
-            lastName.set(fName);
+        
+        public String getAltPair() {
+            return AltPair.get();
         }
-
+        
+        public String getExchanges() {
+            return Exchanges.get();
+        }
+        
+        public String getEndTime() {
+            return EndTime.get();
+        }
+        
+        public String getRunning() {
+            return Running.get();
+        }
+        public String getStartTime() {
+            return StartTime.get();
+        }
     }
 }
