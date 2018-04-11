040e3fe98676a357dd985c45d5dce57270e330de
BatuhanU <batuhan.usluel@gmail.com>
2018-04-11 16:29:47 -0300

    CheckComboBox to Arbitrage
    Added CheckComboBox from ControlsFx to Arbitrage to input multiple exchanges in a better way

--- src/controllers/ArbitrageController.java
+++ src/controllers/ArbitrageController.java
@@ -1,13 +1,31 @@
 package controllers;
 
+import org.controlsfx.control.CheckComboBox;
+
+import application.Exchanges;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 
 public class ArbitrageController {
-    
+    @FXML CheckComboBox<String> ArbitrageExchange;
 	@FXML
     public void initialize(){
-
+		 // create the data to show in the CheckComboBox 
+		 final ObservableList<String> strings = FXCollections.observableArrayList();
+		 for (int i = 0; i < Exchanges.list.size(); i++) {
+		     strings.add(Exchanges.list.get(i));
+		 }
+		 System.out.println("Strings: " + strings);
+		 // Create the CheckComboBox with the data 
+		 ArbitrageExchange.getItems().addAll(strings);
+		 // and listen to the relevant events (e.g. when the selected indices or 
+		 // selected items change).
+		 ArbitrageExchange.getCheckModel().getCheckedItems().addListener(new ListChangeListener<String>() {
+		     public void onChanged(ListChangeListener.Change<? extends String> c) {
+		         System.out.println(ArbitrageExchange.getCheckModel().getCheckedItems());
+		     }
+		 });
+		 }
     }
-	
-
-}
